ca7be95b99f1e98c006483f55686b7c65635f49d
malinovskim <xiiicool@gmail.com>
2020-09-30 16:21:17 +0300

    feat: processFile method for porcessing steam and isAvaibleFile to check the file

--- caesar-cipher-cli/modules/Filer.js
+++ caesar-cipher-cli/modules/Filer.js
@@ -1,10 +1,53 @@
+const fs = require('fs');
+const Cipher = require('../modules/Cipher');
+const { pipeline, Transform } = require('stream');
+const Transformer = require('../modules/Transformer');
+
 /**
  *
  */
 class Filer {
 
+    decodeFile(path){
 
-    isAvaibleFile(file){
+    }
+
+    /**
+     *
+     * @param path
+     * @param shift
+     * @param output
+     */
+    processFile(input, shift, output, action){
+
+        pipeline(
+            fs.createReadStream(input),
+            new Transformer(action,shift),
+            fs.createWriteStream(output, {flags:'a'}),
+            (err) => {
+                if (err) {
+                    console.error('failed.', err);
+                } else {
+                    console.log('succeeded.');
+                }
+            }
+        );
+
+    }
+
+    /**
+     *
+     * @param path
+     */
+    isAvaibleFile(path){
+
+        fs.access(path, fs.F_OK, (err) => {
+            if (err) {
+                console.error(err)
+                return;
+            }
+
+        })
 
     }
 }
