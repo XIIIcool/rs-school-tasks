62bfd2d033ec3200b48aa1d581552ee9d348cf4c
malinovskim <xiiicool@gmail.com>
2020-09-30 16:43:48 +0300

    refactor: refactor code

--- caesar-cipher-cli/caesar-cipher-cli-app.js
+++ caesar-cipher-cli/caesar-cipher-cli-app.js
@@ -1,37 +1,11 @@
-const {program} = require('commander');
 const Cipher = require('./modules/Cipher');
 const Filer = require('./modules/Filer');
+const program = require('./modules/Program');
 const validateActions = ['encode', 'decode'];
 const readline = require('readline');
 const endOfLine = require('os').EOL;
 
-
-program
-    .requiredOption('-a, --actions <type>', ' an action encode/decode')
-    .requiredOption('-s, --shift <number>', ' a shift (integer argument)', (value, dummyPrevious) => {
-        return parseInt(value);
-    })
-    .option('-i, --input <type>', ' an input file')
-    .option('-o, --output <type>', ' an output file')
-
-program.parse(process.argv);
-
-if (isNaN(program.shift)) {
-    // TODO RETURN ERROR need integer value;
-    console.error('error: -s --shift need integer value');
-    throw new Error('error: -s --shift need integer value');
-}
-
-if (program.actions) {
-    if (validateActions.indexOf(program.actions.toLowerCase().trim()) !== -1) {
-
-    } else {
-        console.error('error: -a --actions unknown parameter');
-        throw new Error('error: -a --actions unknown parameter');
-    }
-}
-
-// если не указан входной файл то запросим вводи текста из консоли
+// если не указан входной файл то запросим ввод текста из консоли
 if (!program.input) {
 
     const rl = readline.createInterface({
@@ -53,12 +27,11 @@ if (!program.input) {
 
             console.log(`Your ${program.actions} text: ${result}`);
 
+            // если указан выходной файл то записываем в него
+
     })
 
 
 } else {
-
     Filer.processFile(program.input, program.shift, program.output, program.actions);
-
-
 }
