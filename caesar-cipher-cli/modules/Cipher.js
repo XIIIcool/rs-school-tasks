2c6e72b454933049d270643ef7b56dfc2fca457c
malinovskim <xiiicool@gmail.com>
2020-09-30 16:22:16 +0300

    feat: methods for encode and decode text

--- caesar-cipher-cli/modules/Cipher.js
+++ caesar-cipher-cli/modules/Cipher.js
@@ -1,19 +1,84 @@
 /**
- *
+ * Сipher class
  */
 class Cipher {
 
     constructor() {
 
-    }
-
-    encode(){
+        this.alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
     }
 
-    decode(){
+    /**
+     *
+     * @param text
+     * @param shift
+     * @returns {string}
+     */
+    encode(text, shift) {
+        let correctedAlphabet = this.shiftAlphabert(shift);
+
+        var encryptedMessage = '';
+
+        for (let i = 0; i < text.length; i++) {
+            let indexOfLetter = this.alphabet.indexOf(text[i]);
+
+            if (indexOfLetter === -1) {
+                encryptedMessage = encryptedMessage.concat(text[i]);
+            } else {
+                encryptedMessage = encryptedMessage.concat(correctedAlphabet[indexOfLetter]);
+            }
+
+        }
+
+        return encryptedMessage;
+
+    }
+
+    /**
+     *
+     * @param text
+     * @param shift
+     * @returns {string}
+     */
+    decode(text, shift) {
+        let shiftedAlphabet = this.shiftAlphabert(shift);
+        let encryptedMessage = '';
+        for (let i = 0; i < text.length; i++) {
+            if (text[i] === ' ') {
+                encryptedMessage = encryptedMessage.concat(' ');
+                continue;
+            }
+            let indexOfLetter = shiftedAlphabet.indexOf(text[i]);
+
+            if (indexOfLetter === -1) {
+                encryptedMessage = encryptedMessage.concat(text[i]);
+            } else {
+                encryptedMessage = encryptedMessage.concat(this.alphabet[indexOfLetter]);
+            }
+
+        }
+
+        return encryptedMessage;
+    }
+
+    /**
+     *
+     * @param shift
+     * @returns {string}
+     */
+    shiftAlphabert(shift) {
+
+        var shiftedAlphabet = '';
+        for (var i = 0; i < this.alphabet.length; i++) {
+
+            let currentLetter = (this.alphabet[i + shift] === undefined) ? (this.alphabet[i + shift - this.alphabet.length]) : (this.alphabet[i + shift]);
+
+            shiftedAlphabet = shiftedAlphabet.concat(currentLetter);
+        }
+        return shiftedAlphabet;
 
     }
 }
 
-module.exports = {}
+module.exports = new Cipher();
