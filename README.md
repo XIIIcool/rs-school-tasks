c6d96557068ca80a58dcb7a63553c0fc4de3a7f8
malinovskim <xiiicool@gmail.com>
2020-10-01 13:58:36 +0300

    refactor: refactor code

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# rs-school-tasks
+# rs-school-tasks by @XIIIcool
+
+##Параметры для работы программы caesar-cipher-cli
+```
+-a, --actions обязательный параметр, варианты: encode закодировать и decode разкодировать
+-s, --shift обязательный параметр, целое число
+-i, --input путь к файлу, либо если не задать параметр то будет возможность набрать текст 
+-o, --output путь к файлу, либо если не задать параметр то выходные данные будут выведены в консоли
+```
+
+### Запуск приложения
+Перед запуском приложение установите пакеты - `npm install`
+
+Варианты запуска:
+`node caesar-cipher-cli caesar-cipher-cli-app.js` - если запускаете из корня проекта
+`node caesar-cipher-cli-app.js` - если запускаете из папки с программой
+
+далее примеры показаны с запуском из папки с программой
+
+1. Запускаем кодирование/декодирование из файла с записью в файла
+`node caesar-cipher-cli-app.js --a encode -s 1 -i "input.txt" -o "output.txt"`
+`node caesar-cipher-cli-app.js --a decode -s 1 -i "input.txt" -o "output.txt"`
+2. Запускаем кодирование/декодирование из командной строки с записью в файла
+`node caesar-cipher-cli-app.js --a encode -s 1 -o "output.txt"`
+`node caesar-cipher-cli-app.js --a decode -s 1 -o "output.txt"`
+3. Запускаем кодирование/декодирование из командной строки с записью в командную строку
+`node caesar-cipher-cli-app.js --a encode -s 1 
+`node caesar-cipher-cli-app.js --a decode -s 1 
+4. Запускаем кодирование/декодирование из файла в командную строку
+`node caesar-cipher-cli-app.js --a encode -s 1 -i "input.txt"
+`node caesar-cipher-cli-app.js --a decode -s 1 -i "input.txt"
--- caesar-cipher-cli/caesar-cipher-cli-app.js
+++ caesar-cipher-cli/caesar-cipher-cli-app.js
@@ -1,12 +1,17 @@
 const Cipher = require('./modules/Cipher');
 const Filer = require('./modules/Filer');
 const program = require('./modules/Program');
-const validateActions = ['encode', 'decode'];
 const readline = require('readline');
+const { Readable } = require("stream");
 const endOfLine = require('os').EOL;
 
-// если не указан входной файл то запросим ввод текста из консоли
-if (!program.input) {
+// если указан параметр ввода и вывода
+if(program.input && program.output){
+    Filer.processFile(program.input, program.shift, program.output, program.actions);
+}
+
+// если нету вводных и выводных параметров
+if(!program.input && !program.output){
 
     const rl = readline.createInterface({
         input: process.stdin,
@@ -17,21 +22,36 @@ if (!program.input) {
 
     rl.on('line', (userInput) => {
 
-            let result;
+        let result;
 
-            if(program.actions === 'encode'){
-                result = Cipher.encode(userInput, program.shift);
-            } else if(program.actions === 'decode'){
-                result = Cipher.decode(userInput, program.shift);
-            }
+        if(program.actions === 'encode'){
+            result = Cipher.encode(userInput, program.shift);
+        } else if(program.actions === 'decode'){
+            result = Cipher.decode(userInput, program.shift);
+        }
 
-            console.log(`Your ${program.actions} text: ${result}`);
-
-            // если указан выходной файл то записываем в него
+        console.log(`Your ${program.actions} text: ${result}`);
 
     })
+}
 
+// есть есть вводной параметр и нет выводного
+if(program.input && !program.output) {
+    Filer.processFileReturn(program.input, program.shift, program.output, program.actions);
+}
 
-} else {
-    Filer.processFile(program.input, program.shift, program.output, program.actions);
+// если нет входного но есть выходной
+if(!program.input && program.output) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    console.info('Please write your text. To complete use ctrl+c' + endOfLine)
+
+    rl.on('line', (userInput) => {
+
+        Filer.processStdinToFile(Readable.from([userInput + endOfLine]), program.shift, program.output, program.actions);
+
+    })
 }
